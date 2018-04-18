cb4e619926be719ff84458f616df5b714d2c29b9
eric <zhoufang@ucsd.edu>
2018-04-17 17:05:03 -0700

    test vgg split

--- lib/fast_rcnn/test.py
+++ lib/fast_rcnn/test.py
@@ -18,6 +18,8 @@ import tensorflow as tf
 import os
 from utils.blob import im_list_to_blob
 
+import time
+
 """
 Test a scene graph generation network
 """
@@ -128,13 +130,32 @@ def im_detect(sess, net, inputs, im, boxes, relations, bbox_reg, multi_iter):
     feed_dict[inputs['rel_rois']] = \
         data_utils.compute_rel_rois(num_rel, blobs['rois'], relations)
 
-    ops = {}
+    # Stage 1: VGG feature extraction.
+    ops_vgg = {
+      'conv_out': net.layers['conv_out'],
+    }
+    # Stage 2.
+    ops = {
+      'bbox_deltas': net.bbox_pred_output(multi_iter),
+      'rel_probs': net.rel_pred_output(multi_iter),
+      'cls_probs': net.cls_pred_output(multi_iter)
+    }
 
-    ops['bbox_deltas'] = net.bbox_pred_output(multi_iter)
-    ops['rel_probs'] = net.rel_pred_output(multi_iter)
-    ops['cls_probs'] = net.cls_pred_output(multi_iter)
+    t_start = time.time()
+    ops_vgg_value = sess.run(ops_vgg, feed_dict={
+      inputs['ims']: blobs['data'],
+      net.keep_prob: 1,
+    })
+    print 'VGG takes', time.time() - t_start
 
+    print ops_vgg_value
+
+    del feed_dict[inputs['ims']]
+    feed_dict[ops_vgg['conv_out']] = ops_vgg_value['conv_out']
+
+    t_start = time.time()
     ops_value = sess.run(ops, feed_dict=feed_dict)
+    print 'Scene takes', time.time() - t_start
 
     out_dict = {}
     for mi in multi_iter:
